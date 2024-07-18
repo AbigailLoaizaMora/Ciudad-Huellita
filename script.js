d7ab831373c8263c6fe63c11fdb1c9b447f9648b
AbigailLoaizaMora <abigail.loaiza.mora@gmail.com>
2024-07-18 13:50:38 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+         /* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Aquí puedes añadir validación o autenticación si lo deseas
+    window.location.href = 'ciudad huellita.html';
+});
+
